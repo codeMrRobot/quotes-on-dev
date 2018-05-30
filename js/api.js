b350f26cd878a45271d4192552f027ead9d27229
Tony <diablotony01@hotmail.com>
2018-05-30 14:55:23 -0700

    fixed button style created submit page updated api.js

--- js/api.js
+++ js/api.js
@@ -1,19 +1,45 @@
-(function( $ ) {
-  var $content = $('.hentry');
-  $('#new-quote-button').on('click', function(e){
-    e.preventDefault();
-  
-     $content.empty();
-     $.ajax({
-        method: 'GET',
-        url: api_vars.root_url + '/wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1' 
-     })
-     .done(function(data)) {
-       
+(function($) {
+  $('#new-quote-button').on('click', function(event) {
+      event.preventDefault();
       
+      /**
+       * source https://css-tricks.com/using-the-wp-api-to-fetch-posts/ 05/26/2018
+       */
+      $.ajax({
+          method: 'GET',
+          url: qod_vars.rest_url + 'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+          success: function ( data ) {
+              var quote = data[0].content.rendered;
+              var author = data[0].title.rendered;
+              var quote_Source = data[0]._qod_quote_source;
+              var quote_Source_url = data[0]._qod_quote_source_url;
 
-     });
+              $( '#quote-author' ).text(author);
+              $( '#quote-text' ).html(quote);
+
+              if (quote_Source) {
+                  if (quote_Source_url) {
+                      $('#quote-source').html(', <a href="' + quote_Source_url + '" target="_blank">' + quote_Source + '</a>');
+                  }
+                  else {
+                      $('#quote-source').text(', ' + quote_Source);
+                  }
+              }
+              else {
+                  $('#quote-source').html('');
+              }
+          },
+          cache: false
+      });
+
+  });
+
   
+
+
+
+
+
 })(jQuery);
 
   
